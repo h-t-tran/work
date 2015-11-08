652dc04081f4963fe7b22508e2a18fbc182a11c9
Hoanh Tran <hoanh.tran@gdit.com>
2015-11-08 14:17:50 -0800

    save

--- brownbag/jamine/code/async-timeout-test/tests/CalculatorSpec.js
+++ brownbag/jamine/code/async-timeout-test/tests/CalculatorSpec.js
@@ -16,7 +16,7 @@ describe("Calculator", function() {
 		});
 	});
 
-	describe('asyncAdd', function() {
+	describe('asyncAdd using done callback', function() {
 
 		beforeEach(function() {
 			_calc = new Calculator();
@@ -54,6 +54,38 @@ describe("Calculator", function() {
 			}, 1000);
 
 		}, 4000);
+
+	});
+	describe('asyncAdd  using jasmine.clock', function() {
+
+		beforeEach(function() {
+			_calc = new Calculator();
+			_calc._result = 0;
+			jasmine.clock().install()
+		});
+
+		afterEach(function() {
+			jasmine.clock().uninstall()
+		});
+
+
+		it('should have result equals 5 after 3000 ms', function() {
+			console.debug("1st test.  time ",(new Date()).getTime() / 1000, " sec");
+			_calc.asyncAdd(2, 3);
+			jasmine.clock().tick(3100);
+			expect(_calc._result).toBe(5);
+		});
+
+		//
+		// this test will not start until the done() is called in the first test OR
+		// when 5000 expires
+		//
+		it('should not have result equals 5 before 3000 ms', function() {
+			console.debug("2nd test.  time ",(new Date()).getTime() / 1000, " sec");
+			_calc.asyncAdd(2, 3);
+			jasmine.clock().tick(1000);
+			expect(_calc._result).not.toBe(5);
+		});
 
 	});
 });
