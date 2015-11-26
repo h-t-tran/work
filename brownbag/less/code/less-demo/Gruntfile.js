39545ff95c5ef4d76f21e8cbc68247d4ac7a162d
Hoanh Tran <hoanh.tran@gdit.com>
2015-11-25 22:25:47 -0800

    more less

--- brownbag/less/code/less-demo/Gruntfile.js
+++ brownbag/less/code/less-demo/Gruntfile.js
@@ -29,7 +29,8 @@ module.exports = function(grunt) {
               //
               // Watch for changes of LESS files
               //
-              'app/css/**/*.less'
+              './app/css/**/*.less',
+              "./app/test.less"
           ],
 
 	  tasks: [ 'less' ]
